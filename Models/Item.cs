157362139fcf1001e654f3d289d2c42a02465958
WalterCosseno <161962134+WalterCosseno@users.noreply.github.com>
2025-09-24 22:20:00 -0300

    Update Item.cs

--- Models/Item.cs
+++ Models/Item.cs
@@ -2,7 +2,8 @@ namespace AppParidade.Models
 {
     public class Item
     {
-        public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; } 
     }
 }
